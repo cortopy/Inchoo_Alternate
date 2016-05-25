6e0e5b1ab66b8140e5e58783fb2d4cf265aa4a05
Mikael Rasmussen <m.desmareth@gmail.com>
2016-05-25 15:20:50 +0200

    Update Data.php
    added new method for category url

--- app/code/community/Inchoo/Alternate/Helper/Data.php
+++ app/code/community/Inchoo/Alternate/Helper/Data.php
@@ -14,5 +14,15 @@ class Inchoo_Alternate_Helper_Data extends Mage_Core_Helper_Abstract
         $coreUrl->loadByIdPath($idPath);
         return $coreUrl->getRequestPath();
     }
+    
+    public function rewrittenCategoryUrl($categoryId, $storeId)
+    {
+        $coreUrl = Mage::getModel('core/url_rewrite');
+        $idPath = sprintf('category/%d', $categoryId);
+        $coreUrl->setStoreId($storeId);
+        $coreUrl->loadByIdPath($idPath);
+
+        return $coreUrl->getRequestPath();
+    }
 
 }
